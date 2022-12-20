301afe3156479bac55d51f16edea8183e6fc5c6e
Rene Marceau <rmarceau@c3r7p5.42quebec.com>
2022-12-20 11:44:13 -0500

    commit before vacation

--- src/script.js
+++ src/script.js
@@ -1,10 +1,33 @@
-const   zoom_img = document.getElementsByClassName("zoom_img")
+const   zoom_img = document.getElementsByClassName("zoom_img");
 const   test_header = document.querySelector("#test_header");
+const   player_page = document.querySelector("#player_page");
 const   partners_text = document.querySelector("#text");
 const   test_img = document.getElementsByClassName("test_img");
 const   who_button = document.querySelector("#who_button");
-let     is_on = false;
 const   partners_logo = document.getElementsByClassName("partners_logo");
+
+const   musics = [
+    {
+        artist: "Hamza",
+        image: "../media/hamza_img.webp",
+        music: "",
+        duration: "01:40"
+    },
+    {
+        artist: "Lefa",
+        image: "../media/lefa_img.webp",
+        music: "",
+        duration: "03:12"
+    },
+    {
+        artist: "LuvResval",
+        image: "../media/luvresval.webp",
+        music: "",
+        duration: "01:00"
+    }
+];
+
+let     is_on = false;
 let     zoom = 1.2;
 let     opacity = 0;
 var     scrollMaxY = window.scrollMaxY || (document.documentElement.scrollHeight - document.documentElement.clientHeight);
@@ -18,27 +41,27 @@ window.addEventListener("scroll", () => {
     const   current_scroll = window.pageYOffset;
     const   scroll_pos = current_scroll / scrollMaxY;
     //console.log(opacity);
-    // console.log(scroll_pos);
-    if (current_scroll <= 300) {
+    console.log(scroll_pos);
+    if (scroll_pos <= 0.15) {
         zoom = 1.2 - ((current_scroll / 300) / 5);
     }
     zoom_img[0].style.transform = `scale(${zoom})`;
     zoom_img[1].style.transform = `scale(${zoom})`;
 
-    if (current_scroll >= 300) {
+    if (scroll_pos >= 0.13) {
         test_header.style.animationName = "fade_in_header2";
         test_header.style.animationDelay = "200ms";
         test_header.style.animationDuration = "1500ms";
         test_header.style.animationFillMode = "forwards";
     }
-    if (current_scroll >= 500 && current_scroll <= scrollMaxY - 300) {
+    if (scroll_pos >= 0.17 && scroll_pos <= 0.9) {
         opacity = ((current_scroll) / (scrollMaxY - 300) - 0.3);
     }
     test_img[0].style.opacity = opacity;
     test_img[1].style.opacity = opacity;
     test_img[2].style.opacity = opacity;
 
-        if (current_scroll >= 1200) {
+        if (scroll_pos >= 0.8) {
         partners_text.style.animationName = "partners_animation";
         partners_text.style.animationDelay = "200ms";
         partners_text.style.animationDuration = "1500ms";
@@ -53,22 +76,17 @@ window.addEventListener("scroll", () => {
 
 who_button.addEventListener("click", (e) => {
     e.preventDefault();
-    const   fade_bg = document.createElement("div");
+    var   fade_bg = document.createElement("div");
     is_on = is_on ? false : true;
     if (is_on) {
         fade_bg.id = "fade_bg";
         document.body.appendChild(fade_bg);
+        player_page.style.display = "inherit";
         fade_bg.addEventListener("click", (e) => {
             is_on = false;
-            console.log("lol")
-        })
-        document.body.addEventListener('wheel', preventScroll, {passive: false});
-        function preventScroll(e){
-            e.preventDefault();
-            e.stopPropagation();
-
-            return false;
-        }
+            document.body.removeChild(document.getElementById("fade_bg"));
+            player_page.style.display = "none";
+        });
     } 
     else {
         document.body.removeChild(document.getElementById("fade_bg"));
