2a9320ffa225c28c0a4d66af6f4360a31f0514eb
ReneMarceau <rene.marceau.dev@gmail.com>
2022-12-24 12:15:46 -0500

    Music players all done

--- src/script.js
+++ src/script.js
@@ -4,23 +4,34 @@ const   player_page = document.querySelector("#player_page");
 const   partners_text = document.querySelector("#text");
 const   test_img = document.getElementsByClassName("test_img");
 const   who_button = document.querySelector("#who_button");
+const   exit_button = document.querySelector("#exit_button");
 const   partners_logo = document.getElementsByClassName("partners_logo");
 
 const   musics = [
     {
         artist: "Hamza",
         image: "../public/media/hamza_img.webp",
-        music: "../public/media/music.mp3"
+        music: "../public/media/hamza.wav"
     },
     {
         artist: "Lefa",
         image: "../public/media/lefa_img.webp",
-        music: "../public/media/music.mp3"
+        music: "../public/media/lefa.wav"
+    },
+    {
+      artist: "Hamza",
+      image: "../public/media/hamza_img.webp",
+      music: "../public/media/hamza.wav"
+    },
+    {
+      artist: "Lefa",
+      image: "../public/media/lefa_img.webp",
+      music: "../public/media/lefa.wav"
     },
     {
         artist: "LuvResval",
         image: "../public/media/luvresval_img.webp",
-        music: "../public/media/music.mp3"
+        music: "../public/media/luv.wav"
     }
 ];
 
@@ -46,8 +57,7 @@ window.onbeforeunload = function () {
 window.addEventListener("scroll", () => {
     const   current_scroll = window.pageYOffset;
     const   scroll_pos = current_scroll / scrollMaxY;
-    //console.log(opacity);
-    //console.log(scroll_pos);
+
     if (scroll_pos <= 0.15) {
         zoom = 1.2 - ((current_scroll / 300) / 5);
     }
@@ -88,23 +98,25 @@ who_button.addEventListener("click", (e) => {
         fade_bg.id = "fade_bg";
         document.body.appendChild(fade_bg);
         player_page.style.display = "inherit";
-        fade_bg.addEventListener("click", (e) => {
-            is_on = false;
-            document.body.removeChild(document.getElementById("fade_bg"));
-            player_page.style.display = "none";
-        });
+        fade_bg.addEventListener("click", remove_bg);
+        exit_button.addEventListener("click", remove_bg);
     } 
     else {
         document.body.removeChild(document.getElementById("fade_bg"));
     }
 });
 
-// Player
+function remove_bg() {
+  is_on = false;
+  document.body.removeChild(document.getElementById("fade_bg"));
+  player_page.style.display = "none";
+}
 
-var audioPlayer = document.querySelectorAll('.player_music');
+var audioPlayer = document.querySelector('#players_container');
 var playPause = audioPlayer.querySelectorAll('.playPause');
 var playPauseBtn = audioPlayer.querySelectorAll('.fa-solid');
 var slider = audioPlayer.querySelectorAll('.slider');
+let pin = audioPlayer.querySelectorAll('.pin');
 var progress = audioPlayer.querySelectorAll('.progress');
 var player = audioPlayer.querySelectorAll('audio');
 var currentTime = audioPlayer.querySelectorAll('.current-time');
@@ -114,122 +126,124 @@ var draggableClasses = ['pin'];
 var currentlyDragged = null;
 
 
+  for (let i = 0; i < playPauseBtn.length; i++) {
+    pin[i].addEventListener('mousedown', function(event) {
+      
+        if(!isDraggable(event.target)) return false;
+        
+        currentlyDragged = event.target;
+        
+        window.addEventListener('mousemove', rewind, false);
+        window.addEventListener('mouseup', () => {
+          currentlyDragged = false;
+          window.removeEventListener('mousemove', rewind, false);
+        }, false);  
+      });
 
-window.addEventListener('mousedown', function(event) {
-  
-    if(!isDraggable(event.target)) return false;
     
-    currentlyDragged = event.target;
-    let handleMethod = currentlyDragged.dataset.method;
+      //setInterval(function() {console.log(i)}, 1000);
+      playPauseBtn[i].addEventListener('click', togglePlay);
+      player[i].addEventListener('timeupdate', updateProgress);
     
-    this.addEventListener('mousemove', window[handleMethod], false);
+      player[i].onloadedmetadata = function() {
+        totalTime[i].textContent = formatTime(player[i].duration);
+      };
+    
+      player[i].addEventListener('canplay', makePlay);
+    
+      player[i].addEventListener('ended', function(){
+        playPause[i].attributes.value = "M18 12L0 24V0";
+        player[i].currentTime = 0;
+        playPauseBtn[i].classList.remove("fa-pause");
+        playPauseBtn[i].classList.add("fa-play");
+      });
+    
+      slider[i].addEventListener('click', rewind);
   
-    window.addEventListener('mouseup', () => {
-      currentlyDragged = false;
-      window.removeEventListener('mousemove', window[handleMethod], false);
-    }, false);  
-  });
-
-playPauseBtn.addEventListener('click', togglePlay);
-player.addEventListener('timeupdate', updateProgress);
-player.addEventListener('loadedmetadata', () => {
-  totalTime.textContent = formatTime(player.duration);
-});
-player.addEventListener('canplay', makePlay);
-player.addEventListener('ended', function(){
-  playPause.attributes.value = "M18 12L0 24V0";
-  player.currentTime = 0;
-  playPauseBtn.classList.remove("fa-pause");
-  playPauseBtn.classList.add("fa-play");
-});
-
-let pin = slider.querySelector('.pin');
-slider.addEventListener('click', window[pin.dataset.method]);
-
-function isDraggable(el) {
-  let canDrag = false;
-  let classes = Array.from(el.classList);
-  draggableClasses.forEach(draggable => {
-    if(classes.indexOf(draggable) !== -1)
-      canDrag = true;
-  })
-  return canDrag;
-}
-
-function inRange(event) {
-  let rangeBox = getRangeBox(event);
-  let rect = rangeBox.getBoundingClientRect();
-  let direction = rangeBox.dataset.direction;
-  if(direction == 'horizontal') {
-    var min = rangeBox.offsetLeft;
-    var max = min + rangeBox.offsetWidth;
-  } else {
-      var min = rect.top;
-      var max = min + rangeBox.offsetHeight; 
-    if(event.clientY < min || event.clientY > max) return false;  
+      function isDraggable(el) {
+        let canDrag = false;
+        let classes = Array.from(el.classList);
+        draggableClasses.forEach(draggable => {
+          if(classes.indexOf(draggable) !== -1)
+            canDrag = true;
+        })
+        return canDrag;
+      }
+  
+      function inRange(event) {
+        let rangeBox = getRangeBox(event);
+        let rect = rangeBox.getBoundingClientRect();
+        let direction = rangeBox.dataset.direction;
+        if(direction == 'horizontal') {
+          var min = rangeBox.offsetLeft;
+          var max = min + rangeBox.offsetWidth;
+        } else {
+            var min = rect.top;
+            var max = min + rangeBox.offsetHeight; 
+          if(event.clientY < min || event.clientY > max) return false;  
+        }
+        return true;
+      }
+    
+      function updateProgress() {
+        var current = player[i].currentTime;
+        var percent = (current / player[i].duration) * 100;
+        //console.log(progress[i]);
+        progress[i].style.width = percent + '%';
+        currentTime[i].textContent = formatTime(current);
+      }
+      
+      function getRangeBox(event) {
+        let rangeBox = event.target;
+        let el = currentlyDragged;
+        if(event.type == 'click' && isDraggable(event.target)) {
+          rangeBox = event.target.parentElement.parentElement;
+        }
+        if(event.type == 'mousemove') {
+          rangeBox = el.parentElement.parentElement;
+        }
+        return rangeBox;
+      }
+      
+      function getCoefficient(event) {
+          let slider = getRangeBox(event);
+          let rect = slider.getBoundingClientRect();
+          let K = 0;
+          if(slider.dataset.direction == 'horizontal') {
+            let offsetX = event.clientX - rect.x;
+            let width = slider.clientWidth;
+            K = offsetX / width;
+          }
+          return K;
+        }
+      
+      function rewind(event) {
+        if(inRange(event)) {
+          player[i].currentTime = (player[i].duration * getCoefficient(event));
+        }
+      }
+      
+      function formatTime(time) {
+        var min = Math.floor(time / 60);
+        var sec = Math.floor(time % 60);
+        return min + ':' + ((sec<10) ? ('0' + sec) : sec);
+      }
+    
+      function togglePlay() {
+        if(player[i].paused) {
+          playPause[i].attributes.value = "M0 0h6v24H0zM12 0h6v24h-6z";
+          playPauseBtn[i].classList.remove("fa-play");
+          playPauseBtn[i].classList.add("fa-pause");
+          player[i].play();
+        } else {
+          playPause[i].attributes.value = "M18 12L0 24V0";
+          playPauseBtn[i].classList.remove("fa-pause");
+          playPauseBtn[i].classList.add("fa-play");
+          player[i].pause();
+        }  
+      }
+    
+      function makePlay() {
+        playPauseBtn[i].style.display = 'block';
+      }
   }
-  return true;
-}
-
-function updateProgress() {
-  var current = player.currentTime;
-  var percent = (current / player.duration) * 100;
-  progress.style.width = percent + '%';
-  currentTime.textContent = formatTime(current);
-}
-
-function getRangeBox(event) {
-  let rangeBox = event.target;
-  let el = currentlyDragged;
-  if(event.type == 'click' && isDraggable(event.target)) {
-    rangeBox = event.target.parentElement.parentElement;
-  }
-  if(event.type == 'mousemove') {
-    rangeBox = el.parentElement.parentElement;
-  }
-  return rangeBox;
-}
-
-function getCoefficient(event) {
-    let slider = getRangeBox(event);
-    let rect = slider.getBoundingClientRect();
-    let K = 0;
-    if(slider.dataset.direction == 'horizontal') {
-      let offsetX = event.clientX - rect.x;
-      let width = slider.clientWidth;
-      K = offsetX / width;
-    }
-    console.log(event.clientX - rect);
-    return K;
-  }
-
-function rewind(event) {
-  if(inRange(event)) {
-    //console.log((player.duration * getCoefficient(event)) - 15);
-    player.currentTime = (player.duration * getCoefficient(event));
-  }
-}
-
-function formatTime(time) {
-  var min = Math.floor(time / 60);
-  var sec = Math.floor(time % 60);
-  return min + ':' + ((sec<10) ? ('0' + sec) : sec);
-}
-
-function togglePlay() {
-  if(player.paused) {
-    playPause.attributes.value = "M0 0h6v24H0zM12 0h6v24h-6z";
-    playPauseBtn.classList.remove("fa-play");
-    playPauseBtn.classList.add("fa-pause");
-    player.play();
-  } else {
-    playPause.attributes.value = "M18 12L0 24V0";
-    playPauseBtn.classList.remove("fa-pause");
-    playPauseBtn.classList.add("fa-play");
-    player.pause();
-  }  
-}
-
-function makePlay() {
-  playPauseBtn.style.display = 'block';
-}
